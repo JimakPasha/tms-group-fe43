2ef8beb804bcfed90fe8337ded73ca92c423a8e7
Zhenya-by <PylotEVA02@yandex.ru>
2023-03-17 02:59:59 +0300

    modified: fix module-4, fix module-5

--- homeWork-13/modules-test/module-4.js
+++ homeWork-13/modules-test/module-4.js
@@ -2,4 +2,3 @@ export function addNumbers(num1, num2) {
     return num1 + num2;
   }
   
-  console.log(addNumbers(2, 3));
--- homeWork-13/modules-test/module-5.js
+++ homeWork-13/modules-test/module-5.js
@@ -2,5 +2,5 @@ export function getRandomNumber() {
     return Math.floor(Math.random() * 10) + 1;
   }
   
- // Вызов функции и сохранение результата в переменную
+
  
