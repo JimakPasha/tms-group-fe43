d39b41250480107d05965f89ea3b30dfb2760a50
anmalevich <120416200+anmalevich@users.noreply.github.com>
2023-03-19 20:06:42 +0100

    hanna-malevich-hw12

---
+++ index.js
@@ -0,0 +1,54 @@
+const userName = document.querySelector('.userName');
+const userSurname = document.querySelector('.userSurname');
+const userAge = document.querySelector('.userAge');
+const userInfo = document.querySelector ('.userInfo');
+const createBtn = document.querySelector ('.createBtn');
+const clearBtn = document.querySelector ('.clearBtn');
+
+// Возрастное ограничение
+userAge.addEventListener ('input', () => {
+    createBtn.disabled = userAge.value < 18;
+}); 
+
+
+createBtn.addEventListener ('click', () => {
+
+    const itemObj = {}; // ---------------------------------------------------------------------- Создаем пустой объект
+
+    itemObj.name = userName.value; // ----------------------------------------------------------- Присваиваем значения
+    itemObj.surname = userSurname.value;
+    itemObj.age = userAge.value;
+
+    listArr.push(itemObj); // -------------------------------------------------------------------- Добавляем объекты в массив
+
+    const item = document.createElement('li'); // ------------------------------------------------ Создаем элемент
+    item.textContent = `Пользователь: ${userName.value} ${userSurname.value}, ${userAge.value} лет`;// ------------------------- Добавляем элементу значение
+    userInfo.append(item); // -------------------------------------------------------------------- Добавляем элемент в список
+
+    localStorage.setItem('listArr',JSON.stringify(listArr)); // ---------------------------------- Добавляем данные в LS
+
+});
+
+
+//  Очищение формы
+clearBtn.addEventListener ('click', () => {
+    userName.value = '';
+    userSurname.value = '';
+    userAge.value = '';
+    localStorage.clear();
+    userInfo.remove();
+});
+
+
+// Добавляем данные из LS в лишки
+let listArr = []; 
+
+if (localStorage.getItem('userInfo')){
+    listArr = JSON.parse(localStorage.getItem('userInfo'));
+    listArr.forEach((item) => {
+        const element = document.createElement('li');
+        element.innerText = `Пользователь: ${userName.value} ${userSurname.value}, ${userAge.value} лет`;
+        userInfo.append(element);
+    });
+
+}
