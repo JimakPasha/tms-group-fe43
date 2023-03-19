0ccfd8672af17b101ac95ca8922b3adeb3b281d8
Tanya <120472619+tacherenko@users.noreply.github.com>
2023-03-19 17:51:03 +0100

    feat: add removed from LocalStorage

--- todo-list/script.js
+++ todo-list/script.js
@@ -88,3 +88,15 @@ function getTodos() {
     todoList.appendChild(todoDiv);
   });
 }
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
