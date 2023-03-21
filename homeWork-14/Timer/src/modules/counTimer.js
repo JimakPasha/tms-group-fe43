a4cb76b1e461871ee4345175e863eebbc0c3862a
Dmitriy <Dima.volkov1998@gmail.com>
2023-03-21 15:00:21 +0300

    ref: move variables in function

--- homeWork-14/Timer/src/modules/counTimer.js
+++ homeWork-14/Timer/src/modules/counTimer.js
@@ -1,11 +1,11 @@
-let counter = 0;
-let timer;
 export function count({
   startCountButton: start,
   stopCountButton: stop,
   countTarget: target,
   seconds,
 }) {
+  let timer;
+  let counter = 0;
   start.addEventListener("click", () => {
     timer = setInterval(() => {
       target.textContent = counter++;
