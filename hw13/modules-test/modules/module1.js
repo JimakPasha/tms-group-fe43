c3669712a25e68b8ad792d760e98cb927a021ada
DmitryHash <podolnitsky@gmail.com>
2023-03-21 13:30:22 +0300

    do some home-work13

---
+++ hw13/modules-test/modules/module1.js
@@ -0,0 +1,9 @@
+export function test() {
+    return 1 + 1;
+};
+export function test2() {
+    return 2 + 2;
+};
+export function text3() {
+    return 3 + 3;
+};
---
+++ hw13/modules-test/modules/module2.js
@@ -0,0 +1,12 @@
+export default function omnomnom() {
+    return alert('ЧТо я здесь делаю?');
+};
+export function test4() {
+    return 10 + 10;
+};
+export function test5() {
+    return 100 * 100;
+};
+export function aaaaaa() {
+    return 1234 * 4321;
+};
