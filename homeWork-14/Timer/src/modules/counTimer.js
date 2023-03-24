8f3d9b4af332732103045c8787d332b8374d756a
Dmitriy <Dima.volkov1998@gmail.com>
2023-03-24 16:34:20 +0300

    feat: add sec, 00 check, start once

--- homeWork-14/Timer/src/modules/counTimer.js
+++ homeWork-14/Timer/src/modules/counTimer.js
@@ -3,15 +3,22 @@ export function count({
   stopCountButton: stop,
   countTarget: target,
   seconds,
+  isStart,
 }) {
   let timer;
-  let counter = 0;
+  let counter = 1;
   start.addEventListener("click", () => {
+    if (isStart) return;
     timer = setInterval(() => {
-      target.textContent = counter++;
+      target.textContent = `${addSecconds(counter++)} sec`;
     }, seconds * 1000);
+    isStart = true;
   });
   stop.addEventListener("click", () => {
     clearInterval(timer);
+    isStart = false;
   });
+}
+function addSecconds(num) {
+  return num < 10 ? `0${num}` : num;
 }
