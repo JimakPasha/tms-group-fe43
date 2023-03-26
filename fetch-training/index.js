2b2c92fe70ec4d7e5c985f7d534eeb1e82d14596
Archie <mamchicartem1@gmail.com>
2023-03-26 21:07:51 +0300

    feat: fetch-training

---
+++ fetch-training/index.js
@@ -0,0 +1,28 @@
+const requestBtn = document.querySelector('.request-btn');
+const list = document.querySelector('.list');
+
+function getTodos(){
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/todos/').then((response => {
+            if (response.ok){
+                resolve(response.json());
+            } else {
+                reject(new Error('Some Error'));
+            }
+        }))
+    })
+}
+
+
+function printTodos(callback){
+    callback().then((todos) => {
+        todos.forEach(({id, title}) => {
+            const element = document.createElement('li');
+            element.innerHTML = `${id}. ${title}`
+            list.append(element);
+        })
+        
+    })
+}
+
+requestBtn.addEventListener('click', printTodos(getTodos));
