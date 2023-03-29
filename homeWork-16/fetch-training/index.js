867786e18db9abfd728d75f050eee112ab066751
Dmitriy <Dima.volkov1998@gmail.com>
2023-03-29 22:33:50 +0300

    ref: funcs

--- homeWork-16/fetch-training/index.js
+++ homeWork-16/fetch-training/index.js
@@ -2,11 +2,14 @@ const body = document.getElementsByTagName("body")[0];
 const buttons = document.querySelector(".buttons");
 const nexPage = document.querySelector(".next-page");
 const prevPage = document.querySelector(".prev-page");
+const sortPos = document.querySelector(".sort-positive");
+const sortNeg = document.querySelector(".sort-negative");
 const currPageText = document.querySelector(".current-page");
 const maxPagesText = document.querySelector(".max-pages");
 const contentList = document.querySelector(".list");
 
 let currentEndpoint = "";
+let currentSort = "";
 let currentPage = 1;
 
 currPageText.innerHTML = 0;
@@ -16,37 +19,32 @@ buttons.addEventListener("click", (e) => {
   if (!e.target.classList.contains("get-button")) return;
   const endpoint = e.target.dataset.endpoint;
   currentEndpoint = endpoint;
+  currentPage = 1;
   getTLotRData(endpoint).then((items) => {
-    contentList.innerHTML = "";
-    renderPagesValue(items);
-    pagesCheck(items);
-    endpointCheck(endpoint, items);
+    renderData(endpoint, items);
   });
 });
 
-console.dir(nexPage);
+sortPos.addEventListener("click", () => {});
+
+sortNeg.addEventListener("click", () => {});
 
 nexPage.addEventListener("click", () => {
   currentPage++;
   getTLotRData(currentEndpoint, currentPage).then((items) => {
-    contentList.innerHTML = "";
-    pagesCheck(items);
-    renderPagesValue(items);
-    endpointCheck(currentEndpoint, items);
+    renderData(currentEndpoint, items);
   });
 });
 
 prevPage.addEventListener("click", () => {
   currentPage--;
   getTLotRData(currentEndpoint, currentPage).then((items) => {
-    contentList.innerHTML = "";
-    pagesCheck(items);
-    renderPagesValue(items);
-    endpointCheck(currentEndpoint, items);
+    renderData(currentEndpoint, items);
   });
 });
 
-function endpointCheck(endpoint, context) {
+function renderData(endpoint, context) {
+  contentList.innerHTML = "";
   if (endpoint === "book") {
     context.docs.forEach(({ name }) => {
       renderContent(`Книга: ${name}`);
@@ -66,6 +64,8 @@ function endpointCheck(endpoint, context) {
       renderContent(`Цитата: ${dialog}`);
     });
   }
+  buttonsDisabler(context);
+  correctPagesInfo(context);
 }
 
 function renderContent(content) {
@@ -75,19 +75,30 @@ function renderContent(content) {
   contentList.append(listItem);
 }
 
-function renderPagesValue(context) {
+function correctPagesInfo(context) {
   currPageText.innerHTML = context.page;
   maxPagesText.innerHTML = context.pages;
 }
 
-function pagesCheck(context) {
-  currentPage < context.pages
-    ? (nexPage.disabled = false)
-    : (nexPage.disabled = true);
-  currentPage < 2 ? (prevPage.disabled = true) : (prevPage.disabled = false);
+function buttonsDisabler(context) {
+  {
+    currentPage < context.pages
+      ? (nexPage.disabled = false)
+      : (nexPage.disabled = true);
+  }
+
+  {
+    currentPage < 2 ? (prevPage.disabled = true) : (prevPage.disabled = false);
+  }
+
+  {
+    contentList.childElementCount === 0
+      ? ((sortPos.disabled = true), (sortNeg.disabled = true))
+      : ((sortPos.disabled = false), (sortNeg.disabled = false));
+  }
 }
 
-async function getTLotRData(endpoint, page = 1) {
+async function getTLotRData(endpoint, page = 1, sort = "") {
   const API_KEY = "menxPwb7rM3CjLEboTUT";
   const URL = "https://the-one-api.dev/v2/";
   const headersTLotR = {
@@ -96,7 +107,9 @@ async function getTLotRData(endpoint, page = 1) {
     },
   };
   const response = await fetch(
-    `${URL}${endpoint}?limit=100&page=${page}`,
+    sort === ""
+      ? `${URL}${endpoint}?limit=100&page=${page}`
+      : `${URL}${endpoint}?limit=100&page=${page}&sort=${sort}`,
     headersTLotR
   );
   return response.json();
