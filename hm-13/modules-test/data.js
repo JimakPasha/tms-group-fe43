9eb18e505ad297a4e1917d8b0f87e63f727530ed
AnesTtap <taparova678@gmail.com>
2023-03-30 22:43:15 +0300

    feat: add files

---
+++ hm-13/modules-test/data.js
@@ -0,0 +1,5 @@
+export function getData() {
+    const mounth = data.getMounth()+1;
+    return ` сейчас ${mounth} месяц`
+
+}
---
+++ hm-13/modules-test/sumAge.js
@@ -0,0 +1,9 @@
+export function sumAge(users) {
+    const sumAge = 0;
+    users.map((item) => {
+        sumAge+= item.age
+    });
+    return sumAge;
+    
+}
+
