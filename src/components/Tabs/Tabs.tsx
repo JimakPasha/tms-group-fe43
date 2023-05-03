ae56ed156423d3d0e12730500b6f1c1806f412b0
JimakPasha <jimak-pg@yandex.by>
2023-05-03 21:27:57 +0300

    refactor: add tabsList

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,13 +1,19 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './Tabs.scss';
 import { Tab } from '../Tab/Tab';
 
 export const Tabs: FC = () => {
+    const [tabs, setTabs] = useState([
+        {id: 1, title: 'All', isActive: true, isDisabled: false},
+        {id: 2, title: 'My favorites', isActive: false, isDisabled: false},
+        {id: 3, title: 'Popular', isActive: false, isDisabled: true},
+    ]);
+
     return (
         <ul className='tabs'>
-            <Tab title='All' isActive />
-            <Tab title='My favorites' />
-            <Tab title='Popular' isDisabled />
+            {tabs.map(({id, title, isActive, isDisabled}) => (
+                    <Tab key={id} title={title} isActive={isActive} isDisabled={isDisabled} />
+            ))}
         </ul>
     )
 };
