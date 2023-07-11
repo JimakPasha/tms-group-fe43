3eacec9777e45ea1433b240020951b8a0829845f
JimakPasha <jimak-pg@yandex.by>
2023-07-11 22:54:18 +0300

    fix: delete useState in Tabs component

--- src/pages/BlogPage/components/Tabs/Tabs.tsx
+++ src/pages/BlogPage/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import './Tabs.scss';
 import { Tab } from '../Tab/Tab';
 import { useAppSelector } from '../../../../store/hooks';
